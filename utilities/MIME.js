79d8ee6b52cfb0fdfa7ffb8d953e68cedae163e0
Tyler Washburn <tylermwashburn@gmail.com>
2015-05-29 12:48:33 -0600

    A little bit of cleaning
    So we’re now modulated in an acceptable way and we’re currently passing
    all the tests that have ever been passed. NTS: Make a test suite.

--- utilities/MIME.js
+++ utilities/MIME.js
@@ -2,10 +2,16 @@ var
 fs = require( "fs" ),
 MimeDictionary = function ( map ) {
   this.dictionary = {}
-    
-  String.is( map ) ?
-    this.fromApacheFile( map ) :
+
+  if ( String.is( map ) ) {
+    this.fromApacheFile( map )
+  } else if ( Array.is( map ) ) {
+    map.forEach( function ( ind ) {
+      this.fromApacheFile( ind )
+    }, this )
+  } else {
     this.define.apply( this, arguments )
+  }
 };
 
 MimeDictionary.prototype.define = function ( type, extensions ) {
@@ -30,33 +36,33 @@ MimeDictionary.prototype.define = function ( type, extensions ) {
 
 MimeDictionary.prototype.fromApacheFile = function ( path, encoding, callback ) {
   var dictionary = this;
-  
+
   fs.readFile( path, encoding || "UTF-8", function ( error, content ) {
     var type, extensions;
-    
+
     if ( error ) {
       callback( error )
     } else {
-      // Remove all empty lines and then split the file into lines.
+      // Remove all empty lines and comments and then split the file into lines.
       content = content
-        .replace( /\s*(#.+)?$/m, '' )
+        .replace( /(#.+)$/gm, '' )
         .replace( /\n{2,}/g, "\n" )
         .split( "\n" )
-      
+
       content.forEach(function ( line ) {
-        // Trim off any extra whitespace
+        // Trim off any whitespace on either end of the line
         line = line.trim()
-        
+
         // Ignore any empty lines that made it this far
         if (line) {
           // Remove multiple spaces/tabs and make sure extensions
           // have dots, and then turn it into an array.
-          line = line.replace( /\s+/g, ' .' ) 
+          line = line.replace( /\s+/g, ' .' )
             .split(' ')
-          
+
           type = line.shift() // First item on the line is the mime type
           extensions = line // Everything after is an extension
-          
+
           dictionary.define( type, extensions )
         }
       })
