30a073b77c61c9ccb8922f8f727222c904369e05
Kayla <tylermwashburn@gmail.com>
2017-10-31 21:41:19 -0600

    Testing the waters

--- bin/wv.js
+++ bin/wv.js
@@ -1,4 +1,28 @@
 #!/usr/bin/env node
-let weave = require( '../weave' )
+let weave = require( '..' )
+let readline = require( 'readline' )
 
-weave.flags.enableWeaveRepl()
+let progress = 0
+let width = 50
+let spinner = ['/','-','\\','-']
+
+console.log( 'here we go' )
+
+function start() {
+  process.stdout.write( '[' )
+  process.stdout.write( '-'.repeat( width ) )
+  process.stdout.write( ']' )
+  readline.moveCursor( process.stdout, width * -1 - 1, 0 )
+
+  setTimeout( tick, 1000 )
+}
+
+function tick() {
+  if ( progress >= width ) return;
+
+  process.stdout.write( '+' )
+  progress++
+  setTimeout( tick, 100 )
+}
+
+start()
