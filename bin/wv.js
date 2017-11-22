f4a38cb80fcc8bfbfa7c936a20186c0d379ff75b
Kayla <tylermwashburn@gmail.com>
2017-11-22 14:40:54 -0700

    Messing around

--- bin/wv.js
+++ bin/wv.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 // let weave = require( '..' )
 let readline = require( 'readline' )
+let { fork } = require( 'child_process' )
+let garden = require( 'gardens' ).createGarden( 'blueberry' )
+let { watch } = require( 'fs' )
+let { join } = require( 'path' )
 
 let progress = 0
 let width = 50
 let spinner = ['/','-','\\','-']
-
-console.log( 'here we go' )
+let alive, blueing = true
 
 function start() {
   process.stdout.write( '[' )
@@ -14,15 +17,44 @@ function start() {
   process.stdout.write( ']' )
   readline.moveCursor( process.stdout, width * -1 - 1, 0 )
 
-  setTimeout( tick, 1000 )
+  setTimeout( tick, 500 )
 }
 
 function tick() {
-  if ( progress >= width ) return;
+  if ( progress >= width ) return complete();
 
   process.stdout.write( '-' )
   progress++
-  setTimeout( tick, 100 )
+  setTimeout( tick, 10 )
 }
 
-setTimeout( start, 5000 )
+function complete() {
+  readline.moveCursor( process.stdout, -51, 0 )
+  boot()
+
+  watch( join( __dirname, '..' ), {
+    recursive: true
+  }, reboot )
+
+  watch( process.argv[ 2 ], reboot )
+}
+
+function boot() {
+  alive = fork( process.argv[ 2 ], process.argv.slice( 3 ) )
+  .on( 'exit', code => {
+    if ( code > 0 ) setTimeout( boot, 1000 )
+  })
+  blueing = false
+}
+
+function reboot() {
+  if ( !blueing ) {
+    alive.kill()
+    blueing = true
+    garden.log( 'REBLUEING' )
+    boot()
+  }
+}
+
+// Testing again
+setTimeout( start, 200 )
