8ebd450082e07f44c46c6ba9e881fc5812dd1427
Kayla Washburn <partheseas@gmail.com>
2018-08-31 21:46:44 -0600

    Simplifying further

--- weave.js
+++ weave.js
@@ -6,6 +6,9 @@ const dom = require( './utilities/dom' )
 const dictionaries = require( './utilities/mimedictionary' )
 const gardens = require( 'gardens' )
 
+const http = require( 'http' )
+const https = require( 'https' )
+
 const weave = module.exports = ( ...x ) => new weave.App( ...x )
 
 Object.assign( weave, {
@@ -18,6 +21,38 @@ Object.assign( weave, {
   createDictionary: dictionaries.createDictionary,
 
   _bindings: {},
+
+  bind: ( port, serverOptions ) => {
+    if ( typeof port !== 'number' ) garden.error( 'Not given a port to link to!' )
+    if ( port < 1 || port > 0xFFFF ) return garden.error( `${port} is not a valid port number.` )
+
+    if ( weave._bindings[ port ] ) return weave._bindings[ port ]
+
+    let secure = serverOptions && serverOptions.key && serverOptions.cert
+
+    let binding = {
+      server: ( secure ? https : http ).createServer( serverOptions ),
+      port, attachments: [], hostMatches: {}
+    }
+
+    binding.server.on( 'listening', () => {
+      binding._active = true
+    })
+
+    binding.server.on( 'error', error => {
+      garden.catch( error, 'Unable to bind to port, likely insufficient access.\n'
+        + chalk.green( 'Did you remember to ' ) + 'sudo' + chalk.green( '?' ) );
+      process.exit( 1 );
+    })
+
+    void ['request', 'upgrade'].forEach( event =>
+      binding.server.on( event, ( ...connection ) => new weave.Exchange( secure, ...connection ) ) )
+
+    weave._bindings[ port ] = binding
+    binding.server.listen( port )
+
+    return binding
+  },
 
   configuration: {
     'urlCleaning': true,
