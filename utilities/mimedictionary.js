5ae05b919b3350ed757922fb5d19fe847f9db1a8
Kayla Washburn <partheseas@gmail.com>
2018-10-13 00:11:40 -0600

    Encoding fixes, default dictionary definitions

--- utilities/mimedictionary.js
+++ utilities/mimedictionary.js
@@ -4,7 +4,7 @@
 const fs = require( 'fs' )
 const garden = require( 'gardens' ).createScope( 'weave/MimeDictionary' )
 
-module.exports = class MimeDictionary {
+class MimeDictionary {
   constructor( ...files ) {
     files.forEach( file => {
       if ( typeof file === 'string' ) this.importApacheFile( file )
@@ -21,8 +21,8 @@ module.exports = class MimeDictionary {
       // With multiple extensions
       if ( Array.isArray( extensions ) ) {
         extensions.forEach( extension => {
-          this[ extension ] = type
-        }, this)
+          this[ `.${extension}` ] = type
+        })
       // With a single extension
       } else this[ extensions ] = type
     // We're defining a bunch of types
@@ -55,7 +55,7 @@ module.exports = class MimeDictionary {
           // have dots, and then turn it into an array.
           // First item on the line is the mime type
           // Everything after is an extension
-          let [ type, ...extensions ] = line.replace( /\s+/g, ' .' ).split(' ')
+          let [ type, ...extensions ] = line.replace( /\s+/g, ' ' ).split(' ')
 
           this.define( type, extensions )
         }
@@ -63,3 +63,51 @@ module.exports = class MimeDictionary {
     })
   }
 }
+
+module.exports = MimeDictionary
+module.exports.defaultDictionary = new MimeDictionary().define({
+  'application/ecmascript': [ 'ecma' ],
+  'application/epub+zip': [ 'epub' ],
+  'application/java-archive': [ 'jar' ],
+  'application/javascript; charset=utf-8': [ 'js' ],
+  'application/json; charset=utf-8': [ 'json' ],
+  'application/msword': [ 'doc', 'dot' ],
+  'application/octet-stream': [ 'bin', 'iso', 'dmg', 'pkg' ],
+  'application/pdf': [ 'pdf' ],
+  'application/rtf': [ 'rtf' ],
+  'application/x-bittorrent': [ 'torrent' ],
+  'application/x-bzip': [ 'bz' ],
+  'application/z-bzip2': [ 'bz2' ],
+  'application/x-font-otf': [ 'otf' ],
+  'application/x-font-woff': [ 'woff' ],
+  'application/x-msdownload': [ 'exe', 'dll', 'msi' ],
+  'application/x-rar-compressed': [ 'rar' ],
+  'application/x-sh': [ 'sh' ],
+  'application/xhtml+xml': [ 'xht', 'xhtml' ],
+  'application/xml': [ 'xml' ],
+  'application/xml-dtd': [ 'dtd' ],
+  'application/zip': [ 'zip' ],
+  'audio/mp4': [ 'mp4a' ],
+  'audio/mpeg': [ 'mp2', 'mp2a', 'mp3' ],
+  'audio/ogg': [ 'ogg' ],
+  'audio/webm': [ 'weba' ],
+  'audio/x-aac': [ 'aac' ],
+  'image/bmp': [ 'bmp' ],
+  'image/gif': [ 'gif' ],
+  'image/jpeg': [ 'jpg', 'jpeg' ],
+  'image/png': [ 'png' ],
+  'image/tiff': [ 'tif', 'tiff' ],
+  'image/vnd.adobe.photoshop': [ 'psd' ],
+  'image/webp': [ 'webp' ],
+  'image/x-icon': [ 'ico' ],
+  'text/css; charset=utf-8': [ 'css' ],
+  'text/csv': [ 'csv' ],
+  'text/html; charset=utf-8': [ 'htm', 'html' ],
+  'text/plain': [ 'txt', 'log' ],
+  'video/h264': [ 'h264' ],
+  'video/jpeg': [ 'jpgv' ],
+  'video/mp4': [ 'mp4', 'mp4v', 'mpg4' ],
+  'video/ogg': [ 'ogv' ],
+  'video/quicktime': [ 'mov' ],
+  'video/webm': [ 'webm' ]
+})
